2a56b6ac7528f8e9a999eb5cf850688a794b46e0
João Alberto <hey@joaoalberto.dev>
2022-04-19 11:17:55 -0300

    Animate items based on gravity

--- src/app.js
+++ src/app.js
@@ -35,6 +35,27 @@ function main() {
     };
   }
 
+  function getAngle(X1, Y1, X2, Y2) {
+    const angle = (Math.atan2(Y2 - Y1, X2 - X1) * 180) / Math.PI;
+
+    if (angle < 0) {
+      return 360 + angle;
+    }
+
+    return angle;
+  }
+
+  function animateElement(element, x, y, angle) {
+    console.log(`${Math.floor(angle)}deg`);
+
+    element.setAttribute(
+      "style",
+      `transform: translate3D(${x}px, ${y}px, 0); background: linear-gradient(${Math.floor(
+        angle
+      )}deg,rgba(87, 77, 255, 1) 30%,rgba(207, 0, 255, 1) 100%);`
+    );
+  }
+
   function onMouseMove(event) {
     const { clientX, clientY } = event;
 
@@ -45,7 +66,20 @@ function main() {
         Math.abs(deltaX) <= element.threshold.x &&
         Math.abs(deltaY) <= element.threshold.y
       ) {
-        // Handle animations base on proximity
+        requestAnimationFrame(() => {
+          const angle = getAngle(
+            Math.abs(
+              element.elementPosition.x + element.elementPosition.width / 2
+            ),
+            Math.abs(
+              element.elementPosition.y + element.elementPosition.height / 2
+            ),
+            clientX,
+            clientY
+          );
+
+          animateElement(element.element, deltaX * 0.2, deltaY * 0.2, angle);
+        });
       }
     }
   }
@@ -53,7 +87,7 @@ function main() {
   /**
    * Setup
    */
-  attachElemt(button, { x: 400, y: 100 });
+  attachElemt(button, { x: 400, y: 400 });
 
   /**
    * Event listeners
